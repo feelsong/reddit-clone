ec5f5410f110ad89af171b55260285aadbb48ad3
Tom Benneche <thomas.benneche@uconn.edu>
2012-04-01 18:12:00 -0400

    liferaver rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reddit::Application.config.secret_token = '20eacd41c05ff9482161531ace34c3a04b1812896ecef4900d6f34e6a5816eb8c3031f8a5cd9bfdbcb1e4265a455933365a275d265a6a7f5e8d1df50f52a1680'
